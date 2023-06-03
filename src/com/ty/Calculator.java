2072891cc8ebccdd572cded2d7f27ebe281d6e9e
kiranmv9 <kiranmv53471@gmail.com>
2023-06-03 11:44:50 +0530

    edited

--- src/com/ty/Calculator.java
+++ src/com/ty/Calculator.java
@@ -1,8 +1,39 @@
 package com.ty;
 
 public class Calculator {
+	
+	int balance = 100;
+
+	public void add() {
+		int a = 10;
+		balance = balance + a;
+		System.out.println(balance);
+	}
+
+	public void sub() {
+		int a = 10;
+		balance = balance - a;
+		System.out.println(balance);
+	}
+
+	public void mul() {
+		int a = 10;
+		balance = balance * a;
+		System.out.println(balance);
+	}
+
+	public void div() {
+		int a = 10;
+		balance = balance / a;
+		System.out.println(balance);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("hi");
+		Calculator calculator = new Calculator();
+		calculator.add();
+		calculator.sub();
+		calculator.mul();
+		calculator.div();
 	}
 
 }
